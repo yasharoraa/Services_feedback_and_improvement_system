f1c15a9ff30bc11598b8bc11fc09cb5364ec315c
Yash Arora <yash.arora1704@gmail.com>
2018-07-07 17:52:32 +0530

    fifth commit

--- app/src/main/java/com/hack/innovvapp/Activities/MainActivity.java
+++ app/src/main/java/com/hack/innovvapp/Activities/MainActivity.java
@@ -1,9 +1,6 @@
 package com.hack.innovvapp.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.hack.innovvapp.Fragments.EmitrafeedbackFrament;
 import com.hack.innovvapp.R;
 
 public class MainActivity extends AppCompatActivity
@@ -80,7 +76,10 @@ public class MainActivity extends AppCompatActivity
 
 
 
+
+
         } else if (id == R.id.nav_emitra_feedback) {
+
 
 
         } else if (id == R.id.nav_bhamashah_feedback) {
--- app/src/main/java/com/hack/innovvapp/Fragments/feedbackFrament.java
+++ app/src/main/java/com/hack/innovvapp/Fragments/feedbackFrament.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class EmitrafeedbackFrament extends Fragment {
+public class feedbackFrament extends Fragment {
 
     @Nullable
     @Override
